1a11449f122a155126526a1d92ea048c0108262f
ytwog <ytwog@mail.ru>
2020-04-29 16:13:20 +0700

    Fixed strings

--- tests_1/Unit_test.cpp
+++ tests_1/Unit_test.cpp
@@ -19,119 +19,77 @@ int executeProgram(std::ofstream &ofstr, std::ifstream &ifstr) {
 
 // Проверка на отсутствие ошибок при стандартном раскладе
 TEST(Unit_test, TestNoErrors) {
-    char *arg1 = new char[10]{'i', 'n', 'p', 'u', 't', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', '1', '.', 't', 'x', 't','\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
+    std::ifstream ifstr("input.txt");
+    std::ofstream ofstr("output.txt");
 
     int finalCode = executeProgram(ofstr, ifstr);
     EXPECT_EQ(finalCode, 0);
-    delete [] arg1;
-    delete [] arg2;
 }
 
 // Проверка на обработку ошибки при некорректном вводе метки >text
 TEST(Unit_test, TestError1) {
-    char *arg1 = new char[22]{'t', 'e', 's', 't', 's', '_', '1', '/', 'i', 'n', 'p', 'e', 'r', 'r', '1', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', 'Q', '.', 't', 'x', 't', '\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
-
+    std::ifstream ifstr("tests_1/inperr1.txt");
+    std::ofstream ofstr("output.txt");
     int finalCode = executeProgram(ofstr, ifstr);
     EXPECT_EQ(finalCode, 1);
-    delete [] arg1;
-    delete [] arg2;
 }
 
 // Проверка на обработку ошибки при некорректном вводе метки >replace
 TEST(Unit_test, TestError2) {
-    char *arg1 = new char[22]{'t', 'e', 's', 't', 's', '_', '1', '/', 'i', 'n', 'p', 'e', 'r', 'r', '2', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', 'Q', '.', 't', 'x', 't', '\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
-
+    std::ifstream ifstr("tests_1/inperr2.txt");
+    std::ofstream ofstr("output.txt");
     int finalCode = executeProgram(ofstr, ifstr);
     EXPECT_EQ(finalCode, 2);
-    delete [] arg1;
-    delete [] arg2;
 }
 
 // Проверка на обработку ошибки при некорректном вводе метки >with
 TEST(Unit_test, TestError3) {
-    char *arg1 = new char[22]{'t', 'e', 's', 't', 's', '_', '1', '/', 'i', 'n', 'p', 'e', 'r', 'r', '3', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', 'Q', '.', 't', 'x', 't', '\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
-
+    std::ifstream ifstr("tests_1/inperr3.txt");
+    std::ofstream ofstr("output.txt");
     int finalCode = executeProgram(ofstr, ifstr);
     EXPECT_EQ(finalCode, 3);
-    delete [] arg1;
-    delete [] arg2;
 }
 
 
 // Проверка на обработку ошибки при различии длины исходного и заменяемого алфавита
 TEST(Unit_test, TestError4) {
-    char *arg1 = new char[22]{'t', 'e', 's', 't', 's', '_', '1', '/', 'i', 'n', 'p', 'e', 'r', 'r', '4', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', 'Q', '.', 't', 'x', 't', '\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
-
+    std::ifstream ifstr("tests_1/inperr4.txt");
+    std::ofstream ofstr("output.txt");
     int finalCode = executeProgram(ofstr, ifstr);
     EXPECT_EQ(finalCode, 4);
-    delete [] arg1;
-    delete [] arg2;
 }
 
 // Проверка на обработку ошибки при некорректном вводе метки >type
 TEST(Unit_test, TestError5) {
-    char *arg1 = new char[22]{'t', 'e', 's', 't', 's', '_', '1', '/', 'i', 'n', 'p', 'e', 'r', 'r', '5', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', 'Q', '.', 't', 'x', 't', '\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
-
+    std::ifstream ifstr("tests_1/inperr5.txt");
+    std::ofstream ofstr("output.txt");
     int finalCode = executeProgram(ofstr, ifstr);
     EXPECT_EQ(finalCode, 5);
-    delete [] arg1;
-    delete [] arg2;
 }
 
 
 // Проверка на обработку ошибки
 TEST(Unit_test, TestError6) {
-    char *arg1 = new char[22]{'t', 'e', 's', 't', 's', '_', '1', '/', 'i', 'n', 'p', 'e', 'r', 'r', '6', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', 'Q', '.', 't', 'x', 't', '\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
-
+    std::ifstream ifstr("tests_1/inperr6.txt");
+    std::ofstream ofstr("output.txt");
     int finalCode = executeProgram(ofstr, ifstr);
     EXPECT_EQ(finalCode, 13);
-    delete [] arg1;
-    delete [] arg2;
 }
 
 
 // Проверка на обработку ошибки при вводе некорректного типа
 TEST(Unit_test, TestError9) {
-    char *arg1 = new char[22]{'t', 'e', 's', 't', 's', '_', '1', '/', 'i', 'n', 'p', 'e', 'r', 'r', '9', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', 'Q', '.', 't', 'x', 't', '\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
-
+    std::ifstream ifstr("tests_1/inperr9.txt");
+    std::ofstream ofstr("output.txt");
     int finalCode = executeProgram(ofstr, ifstr);
     EXPECT_EQ(finalCode, 9);
-    delete [] arg1;
-    delete [] arg2;
 }
 
 
 // Проверка на корректность сортировки
 TEST(Unit_test, TestSort) {
-    char *arg1 = new char[22]{'t', 'e', 's', 't', 's', '_', '1', '/', 'i', 'n', 'p', 't', 'e', 's', 't', '.', 't', 'x', 't', '\0'};
-    char *arg2 = new char[12]{'o', 'u', 't', 'p', 'u', 't', 'Q', '.', 't', 'x', 't', '\0'};
-    std::ifstream ifstr(arg1);
-    std::ofstream ofstr(arg2);
-
+    std::ifstream ifstr("tests_1/inptest.txt");
+    std::ofstream ofstr("output.txt");
     if(ifstr.bad() || ifstr.fail() || ofstr.bad() || ofstr.fail()) {
         // Тест провален
         EXPECT_FALSE(true);
@@ -153,6 +111,4 @@ TEST(Unit_test, TestSort) {
     }
     write_to_file(&ofstr, &container);
     ofstr.close();
-    delete [] arg1;
-    delete [] arg2;
 }
